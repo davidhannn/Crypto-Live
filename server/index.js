51bfe69f9064f89cf784abbc8eb409b3805b554f
David Han <davidhan2392@gmail.com>
2021-11-22 10:48:49 -0800

    receiving live update from websocket

--- server/index.js
+++ server/index.js
@@ -1,8 +1,31 @@
 const express = require('express');
-
+const webSocket = require('ws');
 const app = express();
+require('dotenv').config();
+const APIKEY = require('../config.js');
 
 const PORT = 4000;
+
+const wss = new webSocket('wss://ws-sandbox.coinapi.io/v1/');
+
+wss.on('open', function open() {
+  wss.send(JSON.stringify({
+    "type": "hello",
+    "apikey": APIKEY,
+    "heartbeat": false,
+    "subscribe_data_type": ["trade"],
+    "subscribe_filter_symbol_id": [
+      "BITSTAMP_SPOT_BTC_USD$",
+      "BITFINEX_SPOT_BTC_LTC$",
+      "COINBASE_",
+      "ITBIT_"
+      ]
+  }));
+});
+
+wss.on('message', function incoming(message) {
+  console.log('received: %s', message);
+});
 
 
 app.listen(PORT, () => {
